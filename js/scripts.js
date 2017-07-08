323af6dc90ba0f4f7a1da5a392ed25c330b90d07
Kamil Mangold <kamilmangold@gmail.com>
2017-07-08 20:40:15 +0200

    poprawa

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
 $(function() {
 	var carouselList = $("#carousel ul");
-	setInterval(changeSlide, 3000);
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-	});
-	$(function(moveFirstSlide) {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-	});
+	carouselList.animate ({'marginLeft':-200}, 500);
 });
