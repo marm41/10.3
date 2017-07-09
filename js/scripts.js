d985ca14cd1fdacd0aafcd9e66dab6a6ec8834ac
Kamil Mangold <kamilmangold@gmail.com>
2017-07-09 10:57:45 +0200

    ostatnia wersja

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
 $(function() {
 	var carouselList = $("#carousel ul");
-	carouselList.animate ({'marginLeft':-200}, 500);
+	//setInterval(changeSlide, 3000);
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	};
+	function backSlide() {
+		carouselList.animate({'marginLeft':0}, 500);
+	};
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
+	function moveBackSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:-400});
+		backSlide();
+	};
+	$('.left').click(moveBackSlide)
+
+
+	$('.right').click(changeSlide);
 });
